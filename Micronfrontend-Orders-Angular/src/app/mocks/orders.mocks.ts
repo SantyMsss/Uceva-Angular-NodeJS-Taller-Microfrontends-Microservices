5346bc874d666ca3220e7affbfe34e9c59da65d5
Santiago Martinez <santiagomarse@hotmail.com>
2026-04-23 19:46:17 -0500

    docs: add documentation for orders

--- Micronfrontend-Orders-Angular/src/app/mocks/orders.mocks.ts
+++ Micronfrontend-Orders-Angular/src/app/mocks/orders.mocks.ts
@@ -1,5 +1,25 @@
 import { Order } from '../interfaces/orders.interface';
 
+/**
+ * Datos de órdenes de ejemplo para pruebas y desarrollo.
+ * Estos datos se utilizan para simular la respuesta de la API REST
+ * y permitir el desarrollo de la interfaz de usuario sin depender
+ * de un backend real.
+ *
+ * Cada objeto en el array representa una orden con información como
+ * el id, customerName, product, quantity, total, status y date.
+ *
+ * @remarks
+ * Estos datos son estáticos y no cambian durante la ejecución de la aplicación.
+ * Se recomienda utilizarlos solo para propósitos de desarrollo y pruebas, y no deben
+ * ser utilizados en producción.
+ *
+ * @example
+ * ```ts
+ * import { ORDERS_MOCK } from './orders.mocks';
+ * console.log(ORDERS_MOCK);
+ * ```
+ */
 export const ORDERS_MOCK: Order[] = [
   {
     id: 1,
